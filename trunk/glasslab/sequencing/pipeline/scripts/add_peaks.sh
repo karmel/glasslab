d5e2fcc2fae75b472571c433289e38cfd296cf74
Karmel Allison <karmel@arcaio.com>
2012-10-09 10:22:52 -0700

    - Merging back in branch changes

---
+++ trunk/glasslab/sequencing/pipeline/scripts/add_peaks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$CURRENT_PATH" == "" ]; then
+	export CURRENT_PATH="/Users/karmel/Desktop/Projects/GlassLab/Repositories/glasslab/glasslab/trunk"
+fi 
+
+export PYTHONPATH=$CURRENT_PATH:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE=glasslab.config.django_settings
+
+python $CURRENT_PATH/glasslab/sequencing/pipeline/add_peaks.py $@
