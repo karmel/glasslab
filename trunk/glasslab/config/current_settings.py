b3a2bf62d729464acba78ae787724244fc124a52
Karmel Allison <karmel@arcaio.com>
2012-10-09 08:46:42 -0700

    - Trying to fix SourceTree effects...

--- trunk/glasslab/config/current_settings.py
+++ trunk/glasslab/config/current_settings.py
@@ -39,3 +39,17 @@ CHR_LISTS = None # Dynamically set during processing
 # Used to log in as postgres user. Note that authorized keys must be set up!
 PG_ACCESS_CMD = 'ssh postgres@glass.bioinforma.tc' 
 PG_HOME = '/Library/PostgreSQL/9.1'
+
+#####################################
+# Compute resources
+#####################################
+# Used to log in as postgres user. Note that authorized keys must be set up!
+PG_ACCESS_CMD = 'ssh postgres@glass.bioinforma.tc' 
+PG_HOME = '/Library/PostgreSQL/9.1'
+
+#####################################
+# Compute resources
+#####################################
+# Used to log in as postgres user. Note that authorized keys must be set up!
+PG_ACCESS_CMD = 'ssh postgres@glass.bioinforma.tc' 
+PG_HOME = '/Library/PostgreSQL/9.1'
