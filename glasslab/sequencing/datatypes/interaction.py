a8b78fc26a11176c2c34655c9905054e0270ec8b
Karmel Allison <karmel@arcaio.com>
2012-12-08 18:46:58 -0800

    - Interaction uploading

--- glasslab/sequencing/datatypes/interaction.py
+++ glasslab/sequencing/datatypes/interaction.py
@@ -55,7 +55,7 @@ class GlassInteraction(GlassSequencingOutput):
         cls.set_prep_table('prep_%s' % name)
         table_sql = """
         CREATE TABLE "%s" (
-            id varchar(10) defaul null,
+            id varchar(10) default null,
             chromosome_1 varchar(20),
             "start_1" bigint,
             strand_1 int2,
