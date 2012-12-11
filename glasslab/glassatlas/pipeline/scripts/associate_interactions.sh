e91ef245e75a897ada2469f55bc0f5d2eb76b347
Karmel Allison <karmel@arcaio.com>
2012-12-10 19:09:17 -0800

    - Interactions to transcripts script

---
+++ glasslab/glassatlas/pipeline/scripts/associate_interactions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$CURRENT_PATH" == "" ]; then
+	export CURRENT_PATH="/Users/karmel/Desktop/Projects/GlassLab/Repositories/glasslab/glasslab"
+fi 
+
+export PYTHONPATH=$CURRENT_PATH:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE=glasslab.config.django_settings
+
+python $CURRENT_PATH/glasslab/glassatlas/pipeline/associate_interactions.py $@
