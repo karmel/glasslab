d12696a34e63c928b68caa97b210dc0a70ccc1de
Karmel Allison <karmel@arcaio.com>
2013-01-11 16:52:49 -0800

    - HiC analysis

--- glasslab/dataanalysis/misc/interactions_kla_2013/novel_enhancer_me2_change_boxplots.py
+++ glasslab/dataanalysis/misc/interactions_kla_2013/novel_enhancer_me2_change_boxplots.py
@@ -23,53 +23,52 @@ if __name__ == '__main__':
     interactions = interactions.fillna(0)
     
     interactions['hash'] = interactions.apply(lambda row: '{0}.{1}'.format(row['id'],row['id_2']), axis=1)
-    interactions['me2_ratio'] = interactions['me2_kla_6h_tag_count']/nonzero(interactions['me2_notx_tag_count'])
-    interactions['me2_ratio_2'] = interactions['me2_kla_6h_tag_count_2']/nonzero(interactions['me2_notx_tag_count_2'])
     
-    
-    col = 'me2_ratio'
-    pairs = {}
-    pairs['notx'] = interactions[interactions['sequencing_run_id'] == 765]
-    pairs['kla_30m'] = interactions[interactions['sequencing_run_id'] == 766]
-    pairs['kla_4h'] = interactions[interactions['sequencing_run_id'] == 773]
-    
-    
-    # Enhancer is totally new interactor in 4h KLA
-    pairs['only_notx'] = pairs['notx'][(~pairs['notx']['id_2'].isin(pairs['kla_30m']['id_2']))
-                                        & (~pairs['notx']['id_2'].isin(pairs['kla_4h']['id_2']))]
-    pairs['only_kla_30m'] = pairs['kla_30m'][(~pairs['kla_30m']['id_2'].isin(pairs['notx']['id_2']))
-                                         & (~pairs['kla_30m']['id_2'].isin(pairs['kla_4h']['id_2']))]
-    pairs['only_kla_4h'] = pairs['kla_4h'][(~pairs['kla_4h']['id_2'].isin(pairs['notx']['id_2']))
-                                           & (~pairs['kla_4h']['id_2'].isin(pairs['kla_30m']['id_2']))]
-    # Interaction is switched in 4h KLA
-    pairs['only_notx_ixn'] = pairs['notx'][(~pairs['notx']['hash'].isin(pairs['kla_30m']['hash']))
-                                        & (~pairs['notx']['hash'].isin(pairs['kla_4h']['hash']))]
-    pairs['only_kla_30m_ixn'] = pairs['kla_30m'][(~pairs['kla_30m']['hash'].isin(pairs['notx']['hash']))
-                                         & (~pairs['kla_30m']['hash'].isin(pairs['kla_4h']['hash']))]
-    pairs['only_kla_4h_ixn'] = pairs['kla_4h'][(~pairs['kla_4h']['hash'].isin(pairs['notx']['hash']))
-                                            & (~pairs['kla_4h']['hash'].isin(pairs['kla_30m']['hash']))]
-    
-    # Else, existing interaction
-    pairs['shared_notx'] = pairs['notx'][(pairs['notx']['hash'].isin(pairs['kla_30m']['hash']))
-                                        | (pairs['notx']['hash'].isin(pairs['kla_4h']['hash']))]
-    pairs['shared_kla_30m'] = pairs['kla_30m'][(pairs['kla_30m']['hash'].isin(pairs['notx']['hash']))
-                                         | (pairs['kla_30m']['hash'].isin(pairs['kla_4h']['hash']))]
-    pairs['shared_kla_4h'] = pairs['kla_4h'][(pairs['kla_4h']['hash'].isin(pairs['notx']['hash']))
-                                            | (pairs['kla_4h']['hash'].isin(pairs['kla_30m']['hash']))]
-    
-    # Draw boxplots for KLA LFC for enhancers and genes in each set of pairs.
-    labels = ['Enhancers; newly interacting', 'Genes; newly interacting',
-              'Enhancers; switched interaction', 'Genes; switched interaction',
-              'Enhancers; existing', 'Genes; existing',]
-    vals = [pairs['only_kla_4h'][col + '_2'], pairs['only_kla_4h'][col],
-            pairs['only_kla_4h_ixn'][col + '_2'], pairs['only_kla_4h_ixn'][col],
-            pairs['shared_kla_4h'][col + '_2'], pairs['shared_kla_4h'][col],
-            ]
-    
-    labels = [l + '\n(count: {0})'.format(len(v)) for l,v in zip(labels, vals)]
-    title = 'H3K4me2 Tag Count Ratio for Novel Interactions in KLA 4h'
-    ax = yzer.boxplot(vals, labels, 
-                     title=title, xlabel='Transcript subset; interaction state in KLA 4h versus notx and KLA 30m', 
-                     ylabel='log2(me2 in KLA 6h/me2 in notx)', 
-                     show_outliers=False, show_plot=True, wide=True,
-                     save_dir=img_dirpath)
+    for me2_timepoint in ('6h', '24h'):
+        interactions['me2_ratio'] = nonzero(interactions['me2_kla_{0}_tag_count'.format(me2_timepoint)])/\
+                                            nonzero(interactions['me2_notx_tag_count'])
+        interactions['me2_ratio_2'] = nonzero(interactions['me2_kla_{0}_tag_count_2'.format(me2_timepoint)])/\
+                                            nonzero(interactions['me2_notx_tag_count_2'])
+        
+        
+        col = 'me2_ratio'
+        pairs = {}
+        pairs['notx'] = interactions[interactions['sequencing_run_id'] == 765]
+        pairs['kla_30m'] = interactions[interactions['sequencing_run_id'] == 766]
+        pairs['kla_4h'] = interactions[interactions['sequencing_run_id'] == 773]
+        
+        
+        # Enhancer is totally new interactor in 4h KLA
+        pairs['only_notx'] = pairs['notx'][(~pairs['notx']['id_2'].isin(pairs['kla_30m']['id_2']))
+                                            & (~pairs['notx']['id_2'].isin(pairs['kla_4h']['id_2']))]
+        pairs['only_kla_30m'] = pairs['kla_30m'][(~pairs['kla_30m']['id_2'].isin(pairs['notx']['id_2']))]
+        pairs['only_kla_4h'] = pairs['kla_4h'][(~pairs['kla_4h']['id_2'].isin(pairs['notx']['id_2']))]
+        # Interaction is switched in 4h KLA
+        pairs['only_notx_ixn'] = pairs['notx'][(~pairs['notx']['hash'].isin(pairs['kla_30m']['hash']))
+                                            & (~pairs['notx']['hash'].isin(pairs['kla_4h']['hash']))
+                                            & (~pairs['notx']['hash'].isin(pairs['only_notx']['hash']))]
+        pairs['only_kla_30m_ixn'] = pairs['kla_30m'][(~pairs['kla_30m']['hash'].isin(pairs['notx']['hash']))
+                                             & (~pairs['kla_30m']['hash'].isin(pairs['only_kla_30m']['hash']))]
+        pairs['only_kla_4h_ixn'] = pairs['kla_4h'][(~pairs['kla_4h']['hash'].isin(pairs['notx']['hash']))
+                                                & (~pairs['kla_4h']['hash'].isin(pairs['only_kla_4h']['hash']))]
+        
+        # Else, existing interaction
+        pairs['shared_notx'] = pairs['notx'][(pairs['notx']['hash'].isin(pairs['kla_30m']['hash']))
+                                            | (pairs['notx']['hash'].isin(pairs['kla_4h']['hash']))]
+        pairs['shared_kla_30m'] = pairs['kla_30m'][(pairs['kla_30m']['hash'].isin(pairs['notx']['hash']))]
+        pairs['shared_kla_4h'] = pairs['kla_4h'][(pairs['kla_4h']['hash'].isin(pairs['notx']['hash']))]
+        
+        # Draw boxplots for KLA LFC for enhancers and genes in each set of pairs.
+        labels = ['Enhancers; newly interacting', 'Enhancers; switched interaction', 'Enhancers; existing', 
+                  'Genes; newly interacting', 'Genes; switched interaction', 'Genes; existing',]
+        vals = [pairs['only_kla_4h'][col + '_2'], pairs['only_kla_4h_ixn'][col + '_2'], pairs['shared_kla_4h'][col + '_2'],  
+                 pairs['only_kla_4h'][col], pairs['only_kla_4h_ixn'][col], pairs['shared_kla_4h'][col],
+                ]
+        
+        labels = [l + '\n(count: {0})'.format(len(v)) for l,v in zip(labels, vals)]
+        title = 'KLA {0} H3K4me2 Tag Count Ratio for Novel Interactions in KLA 4h'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Transcript subset; interaction state in KLA 4h versus notx', 
+                         ylabel='H3K4me2 Ratio: me2 in KLA {0}/me2 in notx'.format(me2_timepoint), 
+                         show_outliers=False, show_plot=True, wide=True,
+                         save_dir=img_dirpath)
---
+++ glasslab/dataanalysis/misc/interactions_kla_2013/novel_me2_sites.py
@@ -0,0 +1,142 @@
+'''
+Created on Jan 11, 2013
+
+@author: karmel
+
+What do enhancers that are gaining methyl with KLA look like?
+
+'''
+from __future__ import division
+from glasslab.dataanalysis.graphing.seq_grapher import SeqGrapher
+from glasslab.utils.functions import nonzero
+from collections import OrderedDict
+
+if __name__ == '__main__':
+    yzer = SeqGrapher()
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/HiC/'
+    dirpath = yzer.get_path(dirpath)
+    data_dirpath = yzer.get_filename(dirpath, 'enhancer_sets')
+    img_dirpath = yzer.get_and_create_path(dirpath, 'novel_me2_sites')
+ 
+    kla_col='kla_lfc'
+   
+    if False:
+        enhancers = yzer.import_file(yzer.get_filename(data_dirpath,'all_distal_enhancers_inc_me2.txt'))
+        
+        all_transcripts = yzer.import_file(yzer.get_filename(data_dirpath,'transcript_vectors.txt'))
+        transcripts = all_transcripts[['id', kla_col]]
+        enhancers = enhancers.merge(transcripts, how='left', on='id')
+        
+        interactions = yzer.import_file(yzer.get_filename(data_dirpath,'transcript_pairs_refseq_with_me2.txt'))
+        interactions = interactions[interactions['count'] > 1]
+        interactions = interactions.fillna(0)
+        interactions['hash'] = interactions.apply(lambda row: '{0}.{1}'.format(row['id'],row['id_2']), axis=1)
+        
+        # Initialize stats columns
+        for key in ('total_interactions','notx_interactions',
+                    'kla_30m_interactions','kla_4h_interactions',
+                    'kla_30m_over_notx','kla_4h_over_notx'): 
+            enhancers[key] = 0.0 # Don't use int here or else pandas forces the int type.
+        
+        # Add some stats
+        for index, row in enhancers.iterrows():
+            relevant_intxns = interactions[interactions['id_2'] == row['id']]
+            # Straight counts
+            enhancers.ix[index,'total_interactions'] = sum(relevant_intxns.get('norm_count',[]))
+            enhancers.ix[index,'notx_interactions'] = sum(relevant_intxns[relevant_intxns['sequencing_run_id'] == 765].get('norm_count',[]))
+            enhancers.ix[index,'kla_30m_interactions'] = sum(relevant_intxns[relevant_intxns['sequencing_run_id'] == 766].get('norm_count',[]))
+            enhancers.ix[index,'kla_4h_interactions'] = sum(relevant_intxns[relevant_intxns['sequencing_run_id'] == 773].get('norm_count',[]))
+            enhancers.ix[index,'kla_30m_over_notx'] =  (enhancers.ix[index,'kla_30m_interactions'] or 1)/\
+                                                    (enhancers.ix[index,'notx_interactions'] or 1)
+            enhancers.ix[index,'kla_4h_over_notx'] =  (enhancers.ix[index,'kla_4h_interactions'] or 1)/\
+                                                    (enhancers.ix[index,'notx_interactions'] or 1)
+            
+        
+        # Output so that we don't have to recompute that every time.
+        enhancers.to_csv(yzer.get_filename(data_dirpath, 'all_enhancers_with_me2_and_interaction_stats.txt'), 
+                         sep='\t', header=True, index=False)
+    
+    enhancers = yzer.import_file(yzer.get_filename(data_dirpath,'all_enhancers_with_me2_and_interaction_stats.txt'))
+    
+    col = 'me2_ratio'
+    for me2_timepoint in ('6h', '24h'):
+        enhancers[col] = nonzero(enhancers['me2_kla_{0}_tag_count_2'.format(me2_timepoint)])/\
+                                    nonzero(enhancers['me2_notx_tag_count_2'])
+        
+            
+        sets = OrderedDict()
+        sets['2x me2 in KLA {0}'.format(me2_timepoint)] = enhancers[enhancers[col] > 2]
+        sets['No change me2 in KLA {0}'.format(me2_timepoint)] = enhancers[(enhancers[col] >= .5) & (enhancers[col] <= 2)]
+        sets['1/2 me2 in KLA {0}'.format(me2_timepoint)] = enhancers[enhancers[col] < .5]
+        
+        labels = [l + '\n(count: {0})'.format(len(v)) for l,v in zip(sets.keys(), sets.values())]
+        
+        vals = [v[kla_col] for v in sets.values()]
+        title = 'KLA 1h LFC by Ratio of Notx to KLA {0} H3K4me2 for All Enhancers'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='All distal enhancers by relative amount of H3K4me2', 
+                         ylabel='log2(KLA 1h GRO-seq/notx GRO-seq)', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        
+        # Now limit to interacting enhancers
+        for key, val in sets.iteritems(): sets[key] = val[val['total_interactions'] > 0]
+        
+        labels = [l + '\n(count: {0})'.format(len(v)) for l,v in zip(sets.keys(), sets.values())]
+        
+        vals = [v[kla_col] for v in sets.values()]
+        title = 'KLA 1h Log Fold Change by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='log2(KLA 1h GRO-seq/notx GRO-seq)', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        
+        vals = [v['total_interactions'] for v in sets.values()]
+        title = 'Total Interactions with Genes by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Count of interactions with gene TSSs in all conditions', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        
+        
+        vals = [v['notx_interactions'] for v in sets.values()]
+        title = 'Notx Interactions with Genes by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Count of interactions with gene TSSs in notx', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        vals = [v['kla_30m_interactions'] for v in sets.values()]
+        title = 'KLA 30m Interactions with Genes by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Count of interactions with gene TSSs in KLA 30m', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        vals = [v['kla_4h_interactions'] for v in sets.values()]
+        title = 'KLA 4h Interactions with Genes by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Count of interactions with gene TSSs in KLA 4h', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+
+        vals = [v['kla_30m_over_notx'] for v in sets.values()]
+        title = 'Ratio of KLA 30m to Notx Interactions by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Ratio of interactions with gene TSSs in KLA 30m over notx', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        vals = [v['kla_4h_over_notx'] for v in sets.values()]
+        title = 'Ratio of KLA 4h to Notx Interactions by Ratio of Notx to KLA {0} H3K4me2'.format(me2_timepoint)
+        ax = yzer.boxplot(vals, labels, 
+                         title=title, xlabel='Interacting distal enhancers by relative amount of H3K4me2', 
+                         ylabel='Ratio of interactions with gene TSSs in KLA 4h over notx', 
+                         show_outliers=False, show_plot=True, wide=False,
+                         save_dir=img_dirpath)
+        
+        
+        
