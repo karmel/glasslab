d5f17e3169666d0a947d5c85db62d6e1e91b2423
Karmel Allison <karmel@arcaio.com>
2013-03-19 08:35:56 -0700

    - Merging with GlassAtlas

--- glasslab/sequencing/pipeline/add_peaks.py
+++ glasslab/sequencing/pipeline/add_peaks.py
@@ -12,15 +12,14 @@ Run from the command line.
 from glasslab.utils.scripting import GlassOptionParser
 from optparse import make_option
 from glasslab.sequencing.datatypes.peak import GlassPeak
-from glasslab.utils.parsing.delimited import DelimitedFileParser
 from glasslab.sequencing.pipeline.add_short_reads import check_input, \
     create_schema, _print
+from pandas.io.parsers import read_csv
 
-class FastqOptionParser(GlassOptionParser):
+class PeakFileParser(GlassOptionParser):
     options = [
                make_option('-f', '--file_name',action='store', type='string', dest='file_name', 
                            help='Path to FASTQ file for processing.'),
-               make_option('-o', '--output_dir',action='store', type='string', dest='output_dir'),
                make_option('-g', '--genome',action='store', type='string', dest='genome', default='mm9', 
                            help='Currently supported: mm8, mm8r, mm9, hg18, hg18r'),
                make_option('--project_name',action='store', type='string', dest='project_name',  
@@ -44,13 +43,11 @@ def import_peaks(options, file_name, peaks_file_name, peak_type):
     GlassPeak.create_table(file_name)
     #GlassPeak.set_table_name('peak_' + file_name)
     
-    parser = DelimitedFileParser(peaks_file_name)
-    parser.convert_line_endings()
-    data = parser.get_array()
-    if data[0][0] == 'chr':
-        data = data[1:] # Header row
+    if not options.not_homer:
+        data = read_csv(peaks_file_name, sep='\t', header=None, skiprows=40)
+    else: data = read_csv(peaks_file_name, sep='\t', header=None)
     
-    for row in data:
+    for _, row in data.iterrows():
         if not options.not_homer:
             peak = GlassPeak.init_from_homer_row(row)
         else:
@@ -58,9 +55,6 @@ def import_peaks(options, file_name, peaks_file_name, peak_type):
 
         peak.save()
 
-    if options.not_homer and peak_type.diffuse:
-        GlassPeak.score_sicer_peaks()
-    
     GlassPeak.add_indices()
     GlassPeak.add_record_of_tags(peak_type=peak_type)
     
@@ -68,18 +62,13 @@ def import_peaks(options, file_name, peaks_file_name, peak_type):
 if __name__ == '__main__': 
     run_from_command_line = True # Useful for debugging in Eclipse
     
-    parser = FastqOptionParser()
+    parser = PeakFileParser()
     options, args = parser.parse_args()
     
-    # Allow for easy running from Eclipse
-    if not run_from_command_line:
-        options.file_name = '/Users/karmel/GlassLab/SourceData/ThioMac_Lazar/test'
-        options.output_dir = 'first_test'
-        options.peak_table = 'enriched_peaks_enriched_peaks_first_test_2010-09-30_16-04-52_454502'
     
     file_name = check_input(options)
-    
     peaks_file_name = options.file_name
+    parser.set_genome(options)
     
     peak_type = GlassPeak.peak_type(options.peak_type or options.project_name)
     _print('Creating schema if necessary.')
