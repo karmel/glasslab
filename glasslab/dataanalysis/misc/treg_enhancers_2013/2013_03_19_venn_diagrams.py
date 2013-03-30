fb567a17ad35127fa286835ab53bf0eeb095b1d3
Karmel Allison <karmel@arcaio.com>
2013-03-30 09:11:51 -0700

    - Treg enhancers

---
+++ glasslab/dataanalysis/misc/treg_enhancers_2013/2013_03_19_venn_diagrams.py
@@ -0,0 +1,70 @@
+'''
+Created on Feb 12, 2013
+
+@author: karmel
+'''
+from glasslab.dataanalysis.motifs.motif_analyzer import MotifAnalyzer
+
+
+if __name__ == '__main__':
+    yzer = MotifAnalyzer()
+    
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/CD4TCells/TReg_enhancers/2013_03_19'
+    dirpath = yzer.get_path(dirpath)
+    motif_dirpath = yzer.get_filename(dirpath, 'motifs')
+    
+    
+    for antibody in ('me2',):
+        data = {}
+        celltypes = ['treg','naive','th1','th2']
+        for celltype in celltypes:
+            data[celltype] = yzer.import_file(yzer.get_filename(dirpath, 
+                                    '{0}_{1}_versus_others.txt'.format(celltype, 
+                                                                     antibody))).fillna(0)
+                
+            # Filter out promoters
+            data[celltype] = data[celltype][data[celltype]['tss_id'] == 0]
+            
+            # Get venn-diagram "only" sets
+            others = celltypes[:]
+            others.remove(celltype)
+            
+            data[celltype + '_only'] = data[celltype][data[celltype]\
+                                        [[o + '_id' for o in others]].sum(axis=1) == 0]
+            
+            # Pairwise
+            for other in others:
+                other_pair = others[:]
+                other_pair.remove(other)
+                names = sorted([celltype, other])
+                if '_'.join(names) not in data:
+                    data['_'.join(names)] = data[celltype][(data[celltype]\
+                                            [[o + '_id' for o in other_pair]].sum(axis=1) == 0)\
+                                            & (data[celltype][other + '_id'] > 0)]
+                    
+                    # Triplicate
+                    for third in other_pair:
+                        fourth = other_pair[:]
+                        fourth.remove(third)
+                        names = sorted([celltype, other, third])
+                        if '_'.join(names) not in data:
+                            data['_'.join(names)] = data[celltype][(data[celltype]\
+                                                    [[c + '_id' for c in names]].min(axis=1) > 0)\
+                                                    & (data[celltype][fourth[0] + '_id'] == 0)]
+        data['all'] = data['treg'][data['treg']\
+                                   [[c + '_id' for c in celltypes]].min(axis=1) > 0]
+        
+        for k in sorted(data.keys()):
+            subset = data[k] 
+            print k, len(subset)
+            
+            if k in celltypes or len(subset) < 1000: continue
+            first_peak = k.split('_')[0]
+            subset['id'] = subset[first_peak + '_id']
+            subset['start'] = subset[first_peak + '_start']
+            subset['end'] = subset[first_peak + '_end']
+            
+            yzer.run_homer(subset, 'four_way_venn_' + k, motif_dirpath,
+                       cpus=6, center=True, reverse=False, preceding=False, size=200, length=[8, 10, 12, 15])
+
+        '''
