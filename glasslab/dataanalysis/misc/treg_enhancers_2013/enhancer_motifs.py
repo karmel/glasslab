adf09babb832d1749071b4cf5836fd60505ae2f4
Karmel Allison <karmel@arcaio.com>
2013-04-09 09:41:06 -0700

    - Treg enhancers

---
+++ glasslab/dataanalysis/misc/treg_enhancers_2013/enhancer_motifs.py
@@ -0,0 +1,50 @@
+'''
+Created on Mar 28, 2013
+
+@author: karmel
+'''
+from glasslab.dataanalysis.motifs.motif_analyzer import MotifAnalyzer
+
+
+if __name__ == '__main__':
+    yzer = MotifAnalyzer()
+    
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/CD4TCells/TReg_enhancers/2013_04_01'
+    dirpath = yzer.get_path(dirpath)
+    motif_dirpath = yzer.get_filename(dirpath, 'motifs')
+    
+    for antibody in ('me2','ac'):
+        all_treg = yzer.import_file(yzer.get_filename(dirpath, 
+                                    'treg_with_naive_{0}.txt'.format(antibody))).fillna(0)
+        all_naive = yzer.import_file(yzer.get_filename(dirpath,
+                                    'naive_with_treg_{0}.txt'.format(antibody))).fillna(0)
+        
+        # Filter out promoters
+        treg = all_treg[all_treg['tss_id'] == 0]
+        naive = all_naive[all_naive['tss_id'] == 0]
+        
+        # Get venn-diagram sets for foxp3/me2
+        only_treg = treg[treg['naive_id'] == 0]
+        only_naive = naive[naive['treg_id'] == 0]
+        shared = treg[treg['naive_id'] > 0]
+        print len(only_treg), len(only_naive), len(shared)
+        
+        datasets = [all_treg, all_naive, treg, naive, only_treg, only_naive, shared]
+        main_peak = ['treg','naive','treg','naive','treg','naive','treg']
+        names = [x.format(antibody) for x in ('all_treg_{0}_peaks',
+                                              'all_naive_{0}_peaks',
+                                              'all_treg_{0}_enhancers',
+                                              'all_naive_{0}_enhancers',
+                                              'only_treg_{0}_enhancers',
+                                              'only_naive_{0}_enhancers',
+                                              'shared_{0}_enhancers')]
+        for i, subset in enumerate(datasets):
+            if i > 1: continue
+            subset['id'] = subset['{0}_id'.format(main_peak[i])]
+            subset['start'] = subset['{0}_start'.format(main_peak[i])]
+            subset['end'] = subset['{0}_end'.format(main_peak[i])]
+            
+            yzer.run_homer(subset, names[i], motif_dirpath,
+                       cpus=6, center=True, reverse=False, preceding=False, size=200, length=[8, 10, 12, 15])
+
+    
---
+++ glasslab/dataanalysis/misc/treg_enhancers_2013/venn_diagrams.py
@@ -0,0 +1,77 @@
+'''
+Created on Feb 12, 2013
+
+@author: karmel
+'''
+from glasslab.dataanalysis.motifs.motif_analyzer import MotifAnalyzer
+
+
+if __name__ == '__main__':
+    yzer = MotifAnalyzer()
+    
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/CD4TCells/TReg_enhancers/2013_04_01'
+    dirpath = yzer.get_path(dirpath)
+    motif_dirpath = yzer.get_filename(dirpath, 'motifs')
+    
+    
+    for antibody in ('me2','ac'):
+        data = {}
+        celltypes = ['treg','naive','th1','th2']
+        for celltype in celltypes:
+            data[celltype] = yzer.import_file(yzer.get_filename(dirpath, 
+                                    '{0}_{1}_versus_others.txt'.format(celltype, 
+                                                                     antibody))).fillna(0)
+                
+            # Filter out promoters
+            data[celltype] = data[celltype][data[celltype]['tss_id'] == 0]
+            
+            # Get venn-diagram "only" sets
+            others = celltypes[:]
+            others.remove(celltype)
+            
+            data[celltype + '_only'] = data[celltype][data[celltype]\
+                                        [[o + '_id' for o in others]].sum(axis=1) == 0]
+            
+            # Pairwise
+            for other in others:
+                other_pair = others[:]
+                other_pair.remove(other)
+                names = sorted([celltype, other])
+                if '_'.join(names) not in data:
+                    data['_'.join(names)] = data[celltype][(data[celltype]\
+                                            [[o + '_id' for o in other_pair]].sum(axis=1) == 0)\
+                                            & (data[celltype][other + '_id'] > 0)]
+                    
+                    # Triplicate
+                    for third in other_pair:
+                        fourth = other_pair[:]
+                        fourth.remove(third)
+                        names = sorted([celltype, other, third])
+                        if '_'.join(names) not in data:
+                            data['_'.join(names)] = data[celltype][(data[celltype]\
+                                                    [[c + '_id' for c in names]].min(axis=1) > 0)\
+                                                    & (data[celltype][fourth[0] + '_id'] == 0)]
+        
+        data['all'] = data['treg'][data['treg']\
+                                   [[c + '_id' for c in celltypes]].min(axis=1) > 0]
+        
+        # Special cases
+        # Treg and Th1 shared and not shared, regardless of others
+        data['treg_th1_shared'] = data['treg'][data['treg']['th1_id'] > 0]
+        data['treg_not_th1'] = data['treg'][data['treg']['th1_id'] == 0]
+        data['th1_not_treg'] = data['th1'][data['th1']['treg_id'] == 0]
+        
+        for k in sorted(data.keys()):
+            subset = data[k] 
+            print k, len(subset)
+            
+            if k in celltypes or len(subset) < 1000: continue
+            first_peak = k=='all' and 'naive' or k.split('_')[0]
+            subset['id'] = subset[first_peak + '_id']
+            subset['start'] = subset[first_peak + '_start']
+            subset['end'] = subset[first_peak + '_end']
+            
+            if k in ('treg_th1_shared','treg_not_th1','th1_not_treg'):
+                yzer.run_homer(subset, 'four_way_venn_' + k, motif_dirpath,
+                           cpus=6, center=True, reverse=False, preceding=False, size=200, length=[8, 10, 12, 15])
+
