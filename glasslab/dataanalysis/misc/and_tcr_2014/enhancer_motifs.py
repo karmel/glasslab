095c5f3f8dc979d56a221acbeb166067390e40fe
Karmel Allison <karmel@arcaio.com>
2013-12-13 15:35:35 -0800

    Initial analysis of AND ChIP 1

--- glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
+++ glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
@@ -9,23 +9,22 @@ from glasslab.dataanalysis.motifs.motif_analyzer import MotifAnalyzer
 if __name__ == '__main__':
     yzer = MotifAnalyzer()
     
-    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/AND_TCR/Analysis/Chip1/Motifs'
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/AND_TCR/Analysis/Chips1_2/Motifs'
     dirpath = yzer.get_path(dirpath)
     
-    for ab in ('ac',):
+    for ab in ('me2', 'ac'):
         for peptide in ('K99A','NoPep','PCC'):
-        
             pep_dirpath = yzer.get_filename(dirpath, 
                             '{}_{}'.format(peptide, ab))
-            filename = yzer.get_filename(pep_dirpath, 
-                            '{}_{}_enhancers.txt'.format(peptide, ab))
             
-            data = yzer.import_file(filename)
-            data = data.fillna(0)
-
-
-            # me2
-            if True:
+            if False:
+                filename = yzer.get_filename(pep_dirpath, 
+                                '{}_{}_enhancers.txt'.format(peptide, ab))
+                
+                data = yzer.import_file(filename)
+                data = data.fillna(0)
+    
+    
                 yzer.run_homer(data, 
                         'all', pep_dirpath,
                         cpus=6, center=True, reverse=False, preceding=False, 
@@ -35,4 +34,18 @@ if __name__ == '__main__':
                         'tag_thresh_10', pep_dirpath,
                         cpus=6, center=True, reverse=False, preceding=False, 
                         size=200, length=[8, 10, 12, 15], mock=True)
-            
+                
+            if True:
+                filename = yzer.get_filename(pep_dirpath, 
+                                '{}_{}_only_enhancers.txt'.format(peptide, ab))
+                
+                data = yzer.import_file(filename)
+                data = data.fillna(0)
+    
+    
+                # me2
+                if True:
+                    yzer.run_homer(data, 
+                            'only', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
