40df98121198c9a8cd0fbe18e073fa76f74a3130
Karmel Allison <karmel@arcaio.com>
2013-12-18 13:12:47 -0800

    Initial analysis of AND ChIPs 1 and 2

--- glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
+++ glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                 
                 data = yzer.import_file(filename)
                 data = data.fillna(0)
-    
+                
     
                 yzer.run_homer(data, 
                         'all', pep_dirpath,
@@ -35,17 +35,42 @@ if __name__ == '__main__':
                         cpus=6, center=True, reverse=False, preceding=False, 
                         size=200, length=[8, 10, 12, 15], mock=True)
                 
-            if True:
+            if False:
                 filename = yzer.get_filename(pep_dirpath, 
                                 '{}_{}_only_enhancers.txt'.format(peptide, ab))
                 
                 data = yzer.import_file(filename)
                 data = data.fillna(0)
     
-    
-                # me2
                 if True:
                     yzer.run_homer(data, 
                             'only', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
+                    
+            if True and peptide == 'PCC' and ab == 'me2':
+                filename = yzer.get_filename(pep_dirpath, 
+                                '{}_{}_enhancers_batf.txt'.format(peptide, ab))
+                
+                data = yzer.import_file(filename)
+                data = data.fillna(0)
+    
+
+                if True:
+                    subset = data[data['batf_tag_count'] > 0]
+                    yzer.run_homer(subset, 
+                            'with_batf', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
+                    
+                    subset = data[data['irf4_tag_count'] > 0]
+                    yzer.run_homer(subset, 
+                            'with_irf4', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
+                    
+                    subset = subset[subset['batf_tag_count'] > 0]
+                    yzer.run_homer(subset, 
+                            'with_batf_irf4', pep_dirpath,
                             cpus=6, center=True, reverse=False, preceding=False, 
                             size=200, length=[8, 10, 12, 15], mock=True)
