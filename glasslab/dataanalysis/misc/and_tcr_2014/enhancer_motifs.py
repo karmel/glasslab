4443c1f6983565f170a3df034c5e33538176a697
Karmel Allison <karmel@arcaio.com>
2014-06-07 17:03:14 -0700

    Further AND enhancer analysis

--- glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
+++ glasslab/dataanalysis/misc/and_tcr_2014/enhancer_motifs.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
                             cpus=6, center=True, reverse=False, preceding=False, 
                             size=200, length=[8, 10, 12, 15], mock=True)
                     
-            if True and peptide == 'PCC' and ab == 'me2':
+            if False and peptide == 'PCC' and ab == 'me2':
                 filename = yzer.get_filename(pep_dirpath, 
                                 '{}_{}_enhancers_batf.txt'.format(peptide, ab))
                 
@@ -74,3 +74,36 @@ if __name__ == '__main__':
                             'with_batf_irf4', pep_dirpath,
                             cpus=6, center=True, reverse=False, preceding=False, 
                             size=200, length=[8, 10, 12, 15], mock=True)
+
+            if False and peptide == 'PCC':
+                # de novo shared by PCC and K99A
+                filename = yzer.get_filename(pep_dirpath, 
+                                '{}_{}_enhancers_batf.txt'.format(peptide, ab))
+                
+                data = yzer.import_file(filename)
+                data = data.fillna(0)
+    
+                subset = data[data['k99a_tag_count'] > 0]
+                subset = subset[subset['no_pep_tag_count'] == 0]
+                
+                yzer.run_homer(subset, 
+                            'with_k99a_only', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
+                    
+            
+            if True and peptide != 'NoPep':
+                # Newly active PCC and K99A
+                filename = yzer.get_filename(pep_dirpath, 
+                                '{}_{}_enhancers_batf.txt'.format(peptide, ab))
+                
+                data = yzer.import_file(filename)
+                data = data.fillna(0)
+    
+                subset = data[data['no_pep_tag_count'] == 0]
+                
+                yzer.run_homer(subset, 
+                            'not_no_pep', pep_dirpath,
+                            cpus=6, center=True, reverse=False, preceding=False, 
+                            size=200, length=[8, 10, 12, 15], mock=True)
+                    
