e38248953ef8a12307757d348d4873ccc76bc8e8
Karmel Allison <karmel@arcaio.com>
2014-10-02 10:46:25 -0700

    Rodrigo CD8 data

---
+++ glasslab/dataanalysis/misc/rodrigo/sql_atac_lcmv_data.py
@@ -0,0 +1,95 @@
+'''
+Created on Oct 2, 2014
+
+@author: karmel
+
+Output TSV for all of the WT and Foxo1 KO ATAC seq samples from the LCMV
+timecourse.
+
+If running on Mac, make sure psycopg2 is set up:
+sudo ln -s /Library/PostgreSQL/9.2/lib/libssl.1.0.0.dylib /usr/lib
+sudo ln -s /Library/PostgreSQL/9.2/lib/libcrypto.1.0.0.dylib /usr/lib
+
+'''
+import os
+
+from glasslab.dataanalysis.motifs.motif_analyzer import MotifAnalyzer
+from glasslab.utils.database import get_engine, dataframe_from_query
+if __name__ == '__main__':
+    yzer = MotifAnalyzer()
+
+    dirpath = 'karmel/Desktop/Projects/GlassLab/Notes_and_Reports/' +\
+        'Miscellaneous_Collaborations/Rodrigo_CD8s_2014_09/Enhancers_set2'
+    dirpath = yzer.get_path(dirpath)
+
+    # Get DB engine
+    engine = get_engine(
+        uri='ec2-23-20-125-153.compute-1.amazonaws.com',
+        password='rodrigo#cd8')
+
+    # Set up names
+    timepoints = (0, 7, 12, 35)
+    celltypes = ('klrghi', 'klrglo')
+    breeds = ('', 'foxo1_ko_')
+
+    samples = []
+    short_names = []
+    for breed in breeds:
+        for tp in timepoints:
+            if tp == 0:
+                samples.append('{}cd8tcell_atac_lcmv_{}d'.format(
+                    breed, tp))
+                short_names.append('{}d{}'.format(
+                    breed, tp))
+            else:
+                for ct in celltypes:
+                    samples.append('{}cd8tcell_{}_atac_lcmv_{}d'.format(
+                        breed, ct, tp))
+                    short_names.append('{}{}_d{}'.format(
+                        breed, ct, tp))
+
+    # Run and save output from sql queries
+    for i, sample in enumerate(samples):
+        curr_name = short_names[i]
+        others = samples[:i] + samples[i + 1:]
+        oth_names = short_names[:i] + short_names[i + 1:]
+        sql = '''-- {}
+    select distinct on (p1.id)
+    chr."name" as chr_name, p1."start", p1."end", p1.tag_count,
+    p1.*,
+    '''.format(sample)
+        selects, joins = [], []
+        for j, other_sample in enumerate(others):
+            counter = j + 2
+            selects.append(
+                '''p{counter}.tag_count as {}_tag_count'''.format(
+                    oth_names[j], counter=counter))
+            joins.append('''
+    left outer join
+    chipseq.peak_{} p{counter}
+    on p1.chromosome_id = p{counter}.chromosome_id
+    and p1.start_end && p{counter}.start_end
+    '''.format(other_sample, counter=counter))
+
+        sql += ',\n'.join(selects)
+        sql += '''
+    from chipseq.peak_{} p1
+    join genome_reference_mm10.chromosome chr 
+    on p1.chromosome_id = chr.id
+    '''.format(sample)
+        sql += ''.join(joins)
+        sql += '''
+    left outer join genome_reference_mm10.sequence_transcription_region reg
+    on p1.chromosome_id = reg.chromosome_id
+    and p1.start_end && reg.start_site_1000
+    where reg.id is NULL;
+    '''
+        print(sql)
+        # Set up output dir
+        sample_path = yzer.get_and_create_path(dirpath, curr_name)
+
+        # Get data
+        data = dataframe_from_query(sql, engine)
+
+        output_file = yzer.get_path(sample_path, curr_name + '_enhancers.txt')
+        data.to_csv(output_file, sep='\t', header=True, index=False)
--- glasslab/utils/database.py
+++ glasslab/utils/database.py
@@ -1,110 +1,23 @@
 '''
-Created on Nov 15, 2010
+Created on Oct 2, 2014
 
 @author: karmel
 '''
-from django.db import transaction, connections, connection
-from subprocess import check_call
-from glasslab.config import current_settings
-import datetime
-from psycopg2 import OperationalError, Error as psycoError
-import time
+from pandas.io.sql import read_sql_query
+from sqlalchemy import create_engine
 
-def execute_query(query, using='default', return_cursor=False, discard_temp=False):
-    connection = connections[using]
-    connection.close()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    transaction.commit_unless_managed()
-    if return_cursor: return cursor
-    if discard_temp: discard_temp_tables(using=using)
-    connection.close()
 
-def execute_query_without_transaction(query, using='default', return_cursor=False):
-    connection = connections[using]
-    isolation_level = connection.isolation_level
-    connection.close()
-    connection.isolation_level = 0
-    cursor = connection.cursor()
-    cursor.execute(query)
-    transaction.commit_unless_managed()
-    connection.isolation_level = isolation_level
-    if return_cursor: return cursor
-    connection.close()
+def get_engine(uri, password, username='vespucci_user', dbname='vespucci'):
+    '''
+    dialect+driver://username:password@host:port/database
+    '''
+    return create_engine('postgresql://{}:{}@{}:5432/{}'.format(
+        username, password, uri, dbname))
 
-def fetch_rows(query, return_cursor=False, using='default'):
-    connection = connections[using]
-    connection.close()
-    cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    if return_cursor: return result, cursor
-    connection.close()
-    return result
 
-def restart_server():
+def dataframe_from_query(query, engine):
     '''
-    On the Mac server, the Postgres server for some unknown reason
-    does not release memory until restarted. We restart the server programmatically
-    in certain circumstances to force the release of built of memory.
-    
-    To do this, we need to circumvent permissions issues by logging in 
-    as user postgres. So, we ssh in with a public key set up on the 
-    code-runner's machine (karmel@glass.bioinforma.tc, in this case)
-    that is known to the postgres@glass.bioinforma.tc user. Then we restart 
-    the server as the postgres user. 
-    
-    Notably, because we are restarting a daemon process, we have to 
-    redirect all output into dev/null, or else the cursor doesn't return.
-    
-    We want to check first that the server knows it has restarted, and
-    then that it can actually accept incoming queries.
+    Given the database connection and query, return a Pandas dataframe
+    with the requested data.
     '''
-    check_call('{0} "{1}/bin/pg_ctl restart -D {1}/data/ -m immediate </dev/null >/dev/null 2>&1 &"'.format(
-                                            current_settings.PG_ACCESS_CMD,
-                                            current_settings.PG_HOME), shell=True)
-    
-    # Make sure everything went as planned
-    time_to_wait = 5 * 60
-    # We have to wait for the server to actually be up and running!
-    server_is_starting = datetime.datetime.now()
-    while server_is_starting:
-        try:
-            print fetch_rows('SELECT NOW();')
-            connection.close()
-            time.sleep(60)
-            server_is_starting = False
-        except OperationalError, psycoError:
-            if (datetime.datetime.now() - server_is_starting).seconds > time_to_wait:
-                server_is_starting = False
-                raise Exception('Could not run query on server after {0} seconds. Please investigate.'.format(time_to_wait))
-            
-def discard_temp_tables(using='default'):
-    '''
-    Drops all temporary tables created in the current session.
-    '''
-    execute_query_without_transaction('DISCARD TEMP;', using=using)
-    
-    
-    
-class SqlGenerator(object):
-    ''' 
-    Parent class for schema-specific SQL generators.
-    '''
-    user = None
-    def __init__(self, user=None):
-        self.user = user or current_settings.DATABASES['default']['USER']
-    
-    def pkey_sequence_sql(self, schema_name, table_name):
-        return """
-        GRANT ALL ON TABLE "{0}"."{1}" TO  "{user}";
-        CREATE SEQUENCE "{0}"."{1}_id_seq"
-            START WITH 1
-            INCREMENT BY 1
-            NO MINVALUE
-            NO MAXVALUE
-            CACHE 1;
-        ALTER SEQUENCE "{0}"."{1}_id_seq" OWNED BY "{0}"."{1}".id;
-        ALTER TABLE "{0}"."{1}" ALTER COLUMN id SET DEFAULT nextval('"{0}"."{1}_id_seq"'::regclass);
-        ALTER TABLE ONLY "{0}"."{1}" ADD CONSTRAINT {1}_pkey PRIMARY KEY (id);
-        """.format(schema_name, table_name, user=self.user)
+    return read_sql_query(query, engine)
