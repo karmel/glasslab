dd4954b2486a884592b0fd821a3c3e6a40a3bf7f
Karmel Allison <karmel@arcaio.com>
2014-10-05 05:47:28 -0700

    Analysis for Rodrigo CD8s

--- glasslab/dataanalysis/misc/rodrigo/enhancer_motifs_set2.py
+++ glasslab/dataanalysis/misc/rodrigo/enhancer_motifs_set2.py
@@ -13,8 +13,11 @@ if __name__ == '__main__':
         'Miscellaneous_Collaborations/Rodrigo_CD8s_2014_09/Enhancers_set2'
     dirpath = yzer.get_path(dirpath)
 
-    for samples, short_names in get_breed_sets():
-        for sample_prefix in short_names:
+    datasets = {}
+    breed_sets = get_breed_sets()
+    for i, (samples, short_names) in enumerate(breed_sets):
+        oth_breed = breed_sets[1 - i]
+        for j, sample_prefix in enumerate(short_names):
             sample_dirpath = yzer.get_filename(dirpath, sample_prefix)
             filename = yzer.get_filename(sample_dirpath,
                                          sample_prefix + '_enhancers.txt')
@@ -22,12 +25,36 @@ if __name__ == '__main__':
             data = yzer.import_file(filename)
             data = data.fillna(0)
 
-            if True:
+            min_thresh = get_threshold('atac')
 
-                min_thresh = get_threshold('atac')
+            data = data[data['tag_count'] >= min_thresh]
 
-                subdata = data[data['tag_count'] >= min_thresh]
-                yzer.run_homer(subdata,
+            datasets[sample_prefix] = data
+
+            if False:
+
+                yzer.run_homer(data,
                                'all', sample_dirpath,
                                cpus=10, center=True, reverse=False, preceding=False,
                                size=200, length=[8, 10, 12], mock=True)
+            if True:
+                # Versus comparable sample in other breed
+                subdata = data[
+                    data['{}_tag_count'.format(oth_breed[1][j])] < min_thresh]
+                yzer.run_homer(subdata,
+                               'not_in_' + oth_breed[1][j], sample_dirpath,
+                               cpus=10, center=True, reverse=False,
+                               preceding=False,
+                               size=200, length=[8, 10, 12], mock=True)
+
+                # Versus prior sample if not naive.
+                if j > 0:
+                    subdata = data[
+                        data['{}_tag_count'.format(short_names[j - 1])]
+                        < min_thresh]
+                    yzer.run_homer(subdata,
+                                   'not_in_' +
+                                   short_names[j - 1], sample_dirpath,
+                                   cpus=10, center=True, reverse=False,
+                                   preceding=False,
+                                   size=200, length=[8, 10, 12], mock=True)
